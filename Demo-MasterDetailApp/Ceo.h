e8ca980739684d0406d1591cc470a9ecf81c5034
mtuan <tranminhtuan684@gmail.com>
2017-03-14 08:51:08 +0700

    Add Ceo Class to store CEO Data

---
+++ Demo-MasterDetailApp/Ceo.h
@@ -0,0 +1,16 @@
+//
+//  Ceo.h
+//  Demo-MasterDetailApp
+//
+//  Created by ThaoLT on 3/14/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Ceo : NSObject
+
+@property (nonatomic, readwrite, copy) NSString *name;
+@property (nonatomic, readwrite) UIImage *image;
+
+@end
---
+++ Demo-MasterDetailApp/Ceo.m
@@ -0,0 +1,16 @@
+//
+//  Ceo.m
+//  Demo-MasterDetailApp
+//
+//  Created by ThaoLT on 3/14/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import "Ceo.h"
+
+@implementation Ceo
+
+@synthesize name = _name;
+@synthesize image = _image;
+
+@end
